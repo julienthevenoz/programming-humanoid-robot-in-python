bbf5a5b78ba4cb88309944d522f732692ec96e58
JulienThevenoz <julien@thevenoz.ch>
2023-05-24 23:03:50 +0200

    robot pose pkl

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -12,6 +12,11 @@
 
 from angle_interpolation import AngleInterpolationAgent
 from keyframes import hello
+import pickle 
+import os
+import numpy as np #nécessaire ?
+
+
 
 
 class PostureRecognitionAgent(AngleInterpolationAgent):
@@ -22,7 +27,13 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        # LOAD YOUR CLASSIFIER
+        ROBOT_POSE_CLF = 'robot_pose.pkl'
+        self.dir = os.path.dirname(__file__) #ça donne le path qui amène à ce fichier
+        robot_pose_pkl_path = os.path.join(self.dir, ROBOT_POSE_CLF)  #ajoute le path du fichier au .pkl
+        self.posture_classifier = pickle.load(open(robot_pose_pkl_path, 'rb'))
+        # with open(ROBOT_POSE_CLF, 'rb') as nom_fichier:
+        #     self.posture_classifier = pickle.load(nom_fichier)
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
@@ -31,6 +42,30 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
     def recognize_posture(self, perception):
         posture = 'unknown'
         # YOUR CODE HERE
+        
+        joint_list = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+        #angles_data = np.array(len(joint_list) + 2)  #créé un array assez grand pour stocker tous les joints + AngleX & AngleY
+        
+        angles_data = []
+        for joint, jointname in enumerate(joint_list):
+          #print(perception.joint[jointname])
+          angles_data.append(perception.joint[jointname])
+
+        #ajoute les valeurs AngleX & AngleY manuellement à al fin de l'array
+        angles_data.append(perception.imu[0])
+        angles_data.append(perception.imu[1])
+        #changing the shape of array angles data from 1d to 2d so it can be used with predict
+        #angles_data = angles_data.reshape((-1,1))
+        #prediction of the pose
+        pose_predicted = self.posture_classifier.predict([angles_data])
+        #copied from learn_posture.ipynb
+        # ROBOT_POSE_DATA_DIR = 'robot_pose_data' 
+        # classes = listdir(ROBOT_POSE_DATA_DIR)
+        # posture = classes[pose_predicted]
+        
+
+        robot_pose_data_path = os.path.join(self.dir, 'robot_pose_data')
+        posture = os.listdir(robot_pose_data_path)[pose_predicted[0]]
 
         return posture
 
