fb650195215526d1ce40a3ceed6213467e7d1a6d
JulienThevenoz <julien@thevenoz.ch>
2023-05-29 16:54:09 +0200

    je crois que c'est bon pour forward mais jsp comment tester

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -22,6 +22,8 @@ import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
 from numpy.matlib import matrix, identity
+from numpy import cos, sin 
+import numpy as np
 
 from recognize_posture import PostureRecognitionAgent
 
@@ -36,9 +38,20 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
                        # YOUR CODE HERE
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],   #LA DOCU DONNE !R!AnkleRoll
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']  #ET !L!AnkleRoll mais c'est surement une faute alors j'ai corrigé
                        }
+        
+        self.link_translation = {'HeadYaw': [0., 0., 0.1265], 'HeadPitch':	[0., 0., 0.], 
+                                 'LShoulderPitch':	[0., 0.098, 0.1],'LShoulderRoll': [0., 0.,	0.], 'LElbowYaw':	[0.105, 0.015, 0.], 'LElbowRoll':[0., 0., 0.], 
+                                 'RShoulderPitch':	[0., -0.098, 0.1], 'RShoulderRoll': [0., 0.,	0.], 'RElbowYaw':	[0.105, -0.015, 0.], 'RElbowRoll':[0., 0., 0.], 
+                                 'LHipYawPitch': [0., 0.05, -0.085], 'LHipRoll': [0., 0., 0.], 'LHipPitch': [0., 0., 0.], 'LKneePitch': [0., 0., -0.1], 'LAnklePitch': [0., 0., -0.1029], 'LAnkleRoll': [0., 0., 0.],
+                                 'RHipYawPitch': [0, -0.05, -0.085], 'RHipRoll': [0., 0., 0.], 'RHipPitch': [0., 0., 0.],'RKneePitch': [0., 0., -0.1], 'RAnklePitch': [0., 0., -0.1029], 'RAnkleRoll': [0., 0., 0.]  
+                                }
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
@@ -54,7 +67,22 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         '''
         T = identity(4)
         # YOUR CODE HERE
+        c = cos(joint_angle)
+        s = sin(joint_angle)
+        if(joint_name.find('Roll') != -1):                #find returns index of searched string or -1 if not found
+            R = matrix([[1, 0, 0], [0,c,-s], [0, s, c]])  #if we have roll we have Rx matrix
+        if(joint_name.find('Pitch') != -1):    
+            R = matrix([[c, 0, s], [0, 1, 0], [-s, 0, c]]) #pitch -> Ry
+        if(joint_name.find('Yaw') != -1):
+            R = matrix([[c, s, 0], [-s, c, 0], [0, 0, 1]]) #Yaw -> Rz
 
+        T[0:3, 0:3] = R  #insert appropriate rotation matrix in top left of transform matrix
+
+        T[0,3] = self.link_translation[joint_name][0]  #put the translation elements in the transfo matrix
+        T[1,3] = self.link_translation[joint_name][1]
+        T[2,3] = self.link_translation[joint_name][2]
+        
+        
         return T
 
     def forward_kinematics(self, joints):
@@ -68,7 +96,7 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
                 # YOUR CODE HERE
-
+                T = T * Tl              #multiplication matricielle T0_N = T0_1 * T1_2 * T1_3 etc
                 self.transforms[joint] = T
 
 if __name__ == '__main__':
