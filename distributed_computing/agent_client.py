c738232b32d3d633b60855c699952b7eaa00407d
JulienThevenoz <julien@thevenoz.ch>
2023-06-06 16:01:53 +0200

    communication seems to work but transform things don't

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,12 @@
 '''
 
 import weakref
+import xmlrpc.client
+from numpy.matlib import identity
+
+
+#juste pour tester
+from time import sleep
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -27,40 +33,68 @@ class ClientAgent(object):
     '''ClientAgent request RPC service from remote server
     '''
     # YOUR CODE HERE
+
     def __init__(self):
         self.post = PostHandler(self)
+        #crée le proxy
+        self.proxy = xmlrpc.client.ServerProxy('http://localhost:9999', allow_none=True)
+        print('running client')
     
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        return self.proxy.get_angle(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.proxy.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.proxy.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.proxy.execute_keyframes(keyframes)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        self.proxy.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.proxy.set_transform(effector_name, transform)
 
 if __name__ == '__main__':
-    agent = ClientAgent()
-    # TEST CODE HERE
+    try:   #faut virer le try/except et que garder le code dans le try
+        agent = ClientAgent()
+        # TEST CODE HERE
+        
+        agent.set_angle('LElbowYaw', 2)
+        agent.set_angle('LElbowPitch', 1)
+        print(agent.get_angle('LElbowYaw'))
+        print(agent.get_posture())
+
+        #ça ça marche pas encore. Pas étonnant vu que inverse kin c'est de la merde chez moi
+        T = identity(4)
+        T[-1, 0] = -0.5
+        agent.post.set_transform("Head",T)
+        print(agent.get_transform("HeadYaw"))
+
+
+
+    except KeyboardInterrupt:  #ouais bon ça aide pas. 
+        print('exiting')
+        exit(0)
 
 
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -19,39 +19,72 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '
 from inverse_kinematics import InverseKinematicsAgent
 
 
+from xmlrpc.server import SimpleXMLRPCServer,SimpleXMLRPCRequestHandler
+import threading
+
+# restreindre à un certain path. jsp trop ce que je fais là en fait
+# class RequestHandler(SimpleXMLRPCRequestHandler):
+#     rpc_paths = ('/RPC2',)
+
+
 class ServerAgent(InverseKinematicsAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
-    
+    def __init__(self):
+        super(ServerAgent, self).__init__()   #on doit d'abord lancer le truc qui gère la co avec simspark
+        server = SimpleXMLRPCServer(('localhost', 9999), logRequests=True, allow_none=True) #on créé le serveur sur mon ordi au port 9999
+                                                                           #logrequests print les tentaives de connection dans le terminal
+        server.register_instance(self)
+
+        # server.register_introspection_functions()
+        # server.register_multicall_functions()
+        #server.serve_forever()
+        arg_list = []  #pour pouvoir passer des listes au thread
+        self.thread_forever = threading.Thread(target = server.serve_forever, args = arg_list)  #lance le server en tant que thread
+        self.thread_forever.start()  #useful ?
+        print('server is active')
+
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
         # YOUR CODE HERE
+        if joint_name in self.perception.joint:
+            return str(self.perception.joint.get(joint_name)) #NB we return a string
+        else:
+            print('invalid joint name')
+            return 'invalid joint name'
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
         # YOUR CODE HERE
+        self.target_joints[joint_name] = angle
+
 
     def get_posture(self):
         '''return current posture of robot'''
         # YOUR CODE HERE
+        return self.recognize_posture(self.perception)  #returns a string with the posture
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
         # YOUR CODE HERE
+        self.keyframes = keyframes 
 
     def get_transform(self, name):
         '''get transform with given name
         '''
         # YOUR CODE HERE
+        return self.transforms[name]
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
         # YOUR CODE HERE
+        self.set_transforms(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ServerAgent()
